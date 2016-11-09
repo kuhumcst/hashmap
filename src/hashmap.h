0fcb7a9db335bb6aaf0ef0bc1cb00268675e4a2a
Bart Jongejan <bart@cst.dk>
2016-11-09 11:14:28 +0100

    Added #ifndef macro to mitigate effect of multiple inclusion

--- src/hashmap.h
+++ src/hashmap.h
@@ -19,6 +19,8 @@ You should have received a copy of the GNU General Public License
 along with CSTHASH; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+#ifndef HASHMAP_H
+#define HASHMAP_H
 
 #include <assert.h>
 #include <stddef.h>
@@ -271,3 +273,4 @@ template <class record> class chainLink
     };
 
 }
+#endif
